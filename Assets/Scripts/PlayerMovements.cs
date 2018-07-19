8563f1fb6d87c1eff17696bfd907e530b07f7e05
Tushar <wtushar09@gmail.com>
2018-07-20 01:06:34 +0600

    initialize

---
+++ Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour {
+
+	public float moveSpeed = 10;
+	public float jumpSpeed = 10;
+	public float tempSpeed;
+	public float accSpeed;
+
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
