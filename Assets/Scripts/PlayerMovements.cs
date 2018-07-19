fbf2e5a08e108ddf83734eefaa232c214682ac14
Tushar <wtushar09@gmail.com>
2018-07-20 01:12:01 +0600

    left right move code is done

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -19,6 +19,24 @@ public class PlayerMovements : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		//left
+		if(Input.GetKey(KeyCode.A)){
+			rb.velocity = new Vector2 (-moveSpeed, rb.velocity.y);
+		}
+		//left stop
+		if(Input.GetKeyUp(KeyCode.A)){
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
+
+
+		//Right
+		if(Input.GetKey(KeyCode.D)){
+			rb.velocity = new Vector2 (moveSpeed, rb.velocity.y);
+		}
+		//left stop
+		if(Input.GetKeyUp(KeyCode.D)){
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
 	}
 }
