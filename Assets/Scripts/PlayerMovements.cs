fd6380a747916467b8006df737061c6ee1657a01
Tushar <wtushar09@gmail.com>
2018-07-20 01:39:26 +0600

    running code is done

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class PlayerMovements : MonoBehaviour {
 
-	public float moveSpeed = 10;
-	public float jumpSpeed = 10;
+	public float moveSpeed = 10f;
+	public float jumpSpeed = 10f;
 	public float tempSpeed;
-	public float accSpeed;
+	public float accSpeed = 15f;
 
 	public Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
-
+		tempSpeed = moveSpeed;
+		accSpeed = 15f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		//Debug.Log ("speed " + moveSpeed);
 
 		//left
 		if(Input.GetKey(KeyCode.A)){
@@ -37,6 +40,21 @@ public class PlayerMovements : MonoBehaviour {
 		//left stop
 		if(Input.GetKeyUp(KeyCode.D)){
 			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
+
+
+		//run
+		if(Input.GetKey(KeyCode.LeftShift)){
+			Debug.Log ("asdf" + moveSpeed);
+			if (moveSpeed < accSpeed) {
+				moveSpeed += accSpeed / 120;
+			} else {
+				Debug.Log ("smaller " + moveSpeed + " " + accSpeed);
+			}
+		}
+		//left stop
+		if(Input.GetKeyUp(KeyCode.LeftShift)){
+			moveSpeed = tempSpeed;
 		}
 	}
 }
