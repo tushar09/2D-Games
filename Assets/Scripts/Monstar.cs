10be06fd0fbd5a2f0239ae9d569a64a95e8bd899
Tushar <wtushar09@gmail.com>
2018-07-21 15:51:42 +0600

    new monstar added

---
+++ Assets/Scripts/Monstar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monstar : MonoBehaviour {
+
+	void OnTriggerEnter2D(){
+		Debug.Log ("enters");
+	}
+
+	void OnTriggerExit2D(){
+		
+	}
+}
